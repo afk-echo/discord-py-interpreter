0f95538bc403de45d3d34ecd445bf3e28585d1ab
afk-echo <58026651+afk-echo@users.noreply.github.com>
2021-07-29 15:20:40 +0530

    Runs the user code in a separate console window.

--- main.py
+++ main.py
@@ -3,15 +3,55 @@ import sys, os
 from discord.ext import commands
 from ConsoleToStr import ConsoleToStrConverter
 
+# creates a Bot with command prefix p. - can be changed to whatever you like.
 bot = commands.Bot(command_prefix="p.")
-conv = ConsoleToStrConverter()
 
+# A helper function for execution of the code. Helps in making the code a bit more modular.
+def run_in_diff_console(code):
+    
+    '''
+    A helper function that helps execute the code given as an argument to the bot.
+    '''
+    
+    # generates 2 files - tempcode.py and output.txt, from where the code fed by the user will be executed and stored respectively.
+    with open("tempcode.py","w") as f:
+        f.write("from ConsoleToStr import ConsoleToStrConverter\n")
+        f.write("conv = ConsoleToStrConverter()\n")
+        f.write("conv.start()\n")
+        f.write(code)
+        f.write("result = conv.stop()\n")
+        f.write("""
+with open('output.txt','w') as f:
+    f.write(result)
+""")
+    
+    os.system("python tempcode.py")
+    result = ""
+    
+    with open("output.txt","r") as f:
+        line = " "
+        while line:
+            line = f.readline()
+            result += line
+    return result
+ 
+# Actual bot code starts here.
+
+# Prints the bot's username when the bot is ready to use.
 @bot.event
 async def on_ready():
     print(f"Bot {bot.user.name}#{bot.user.discriminator} logged in!")
 
+# The main function that allows you to run the code fed by a user.
 @bot.command()
 async def run(ctx):
+    '''
+    Input format for the code:
+    
+    <prefix>.run ```python
+     --enter code here--
+    ```
+    '''
     code_to_be_exec = ctx.message.content.lstrip("p.run").lstrip(" ```python").rstrip("```")
     code_to_be_exec.replace("os.remove","forbidden_command=")
     conv.start()
@@ -20,10 +60,16 @@ async def run(ctx):
     await ctx.send(f"Result is: ```\n{result}```")
 @run.error
 async def run_error(ctx, error):
+    '''
+    Returns the errors raised by the bot as a whole(including the ones raised by the user's code.)
+    '''
     await ctx.send(f"```{str(error)}```")
 
 @bot.command()
 async def files(ctx):
+    '''
+    Returns all the files stored in the bot's working directory.
+    '''
     finalstr = ""
     for i in os.listdir():
         finalstr += i + "\n"
@@ -31,6 +77,9 @@ async def files(ctx):
 
 @bot.command()
 async def remove(ctx, file):
+    '''
+    Removes a file from the bot's working directory.
+    '''
     if file in os.listdir():
         os.remove(file)
     else:
